9d4024ac1d9693e46282528402041ad175f86033
GitHalex <calex1731@gmail.com>
2024-03-27 21:52:37 -0400

    agregando mas ejerccios para practicar

---
+++ exercices-level-1/exercice-1.py
@@ -0,0 +1,7 @@
+'''Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
+Es cierto que python tiene una función max() incorporada, pero hacerla nosotros mismos es un
+muy buen ejercicio'''
+number_1 = int(input("first number:"))
+number_2 = int(input("second number:"))
+maximo = max(number_1, number_2)
+print(maximo)
