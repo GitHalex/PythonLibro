cafdfdc3fa0cbf2c88dd4de54ea33b4021077344
GitHalex <calex1731@gmail.com>
2024-03-29 18:15:45 -0400

    exercicio2 de nivel 1 terminado

--- exercices-level-1/exercice-2.py
+++ exercices-level-1/exercice-2.py
@@ -1,2 +1,10 @@
 '''Definir una función max_de_tres(), que tome tres números como argumentos y devuelva el
 mayor de ellos. '''
+
+
+def max_de_tres(num1, num2, num3):
+    print(
+        f"el numero mayor de {num1}-{num2}-{num3} es: {max(num1, num2, num3)}")
+
+
+max_de_tres(1, 2, 3)
