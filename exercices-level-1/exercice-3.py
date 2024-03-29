6b7c5f9152f9e35da8406ca0b7e0b3cd64011da9
GitHalex <calex1731@gmail.com>
2024-03-29 18:21:23 -0400

    exercicio 2 del nivel uno terminado

--- exercices-level-1/exercice-3.py
+++ exercices-level-1/exercice-3.py
@@ -1,3 +1,12 @@
 '''Definir una función que calcule la longitud de una lista o una cadena dada. (Es cierto que python
 tiene la función len() incorporada, pero escribirla por nosotros mismos resulta un muy buen
 ejercicio.'''
+
+
+def longitud(cadena):
+    longitud_cadena = len(cadena)
+    return longitud_cadena
+
+
+cadena = input("Please input a String please: ")
+print(f"El tamaño de la cadena es: {longitud(cadena)} caracteres")
