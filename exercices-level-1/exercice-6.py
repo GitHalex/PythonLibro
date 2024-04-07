b855f68dd8eb6ee06ede00e74661b7c0f251b966
GitHalex <calex1731@gmail.com>
2024-04-07 09:12:08 -0400

    corrigiendo errrores

--- exercices-level-1/exercice-6.py
+++ exercices-level-1/exercice-6.py
@@ -1,2 +1,14 @@
 '''Definir una función inversa() que calcule la inversión de una cadena. Por ejemplo la cadena
 "estoy probando" debería devolver la cadena "odnaborp yotse"'''
+
+
+def inversa(cadena):
+    revertido = ""
+    i = len(cadena)
+    while i >= 0:
+        revertido += cadena[i]
+        i += 1
+    return revertido
+
+
+print(inversa("hola"))
