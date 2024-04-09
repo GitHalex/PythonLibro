b98b8df95ec31a832861b466f818bc19db3c335f
GitHalex <calex1731@gmail.com>
2024-04-08 20:06:28 -0400

    exercice 9 terminado del nivel uno

--- exercices-level-1/exercice-9.py
+++ exercices-level-1/exercice-9.py
@@ -1,2 +1,9 @@
 '''Definir una función generar_n_caracteres() que tome un entero n y devuelva el caracter
 multiplicado por n. Por ejemplo: generar_n_caracteres(5, "x") debería devolver "xxxxx".'''
+
+
+def generar_n_caracteres(num, char):
+    return char*num
+
+
+print(generar_n_caracteres(5, "x"))
