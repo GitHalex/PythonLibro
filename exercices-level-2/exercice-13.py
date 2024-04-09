16bcebbbe37127c7e1d926a08ace2ba66e0dab18
GitHalex <calex1731@gmail.com>
2024-04-08 20:19:35 -0400

    ejercicio 13 del nivel dos terminado

--- exercices-level-2/exercice-13.py
+++ exercices-level-2/exercice-13.py
@@ -1,3 +1,13 @@
 '''Escribir un programa que pregunte el nombre del usuario en la consola
  y después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!,
  donde <nombre> es el nombre que el usuario haya introducido.'''
+
+name = input("Please type your name: ")
+
+
+def myName(name):
+    return f"¡Hola {name}!"
+
+
+print(myName(name))
+print(myName(name))
