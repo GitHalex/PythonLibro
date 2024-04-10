87c25329563b45ca7256cb41a2080332dffe4c28
GitHalex <calex1731@gmail.com>
2024-04-09 22:58:55 -0400

    ejercicio 14 de nivel 2 terminado

--- exercices-level-2/exercice-14.py
+++ exercices-level-2/exercice-14.py
@@ -1,2 +1,6 @@
 '''Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. 
 Después debe mostrar por pantalla la paga que le corresponde.'''
+horas_trabajadas = int(input("Cantidad de horas que ah trabajado: "))
+costo_hora = int(input("Costo por hora de trabajo: "))
+costo_por_trabajo = horas_trabajadas*costo_hora
+print(f"ganaste por las horas de trabajo: {costo_por_trabajo} pelucholares")
