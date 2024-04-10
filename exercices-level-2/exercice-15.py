2b415d8ddf45045a1345ba52e234940347aa6c27
GitHalex <calex1731@gmail.com>
2024-04-09 23:05:04 -0400

    ejercicio 15 terminado del nivel dos

--- exercices-level-2/exercice-15.py
+++ exercices-level-2/exercice-15.py
@@ -3,3 +3,13 @@
   y muestre por pantalla la frase Tu índice de masa corporal es <imc> donde <imc> es el
    índice de masa corporal calculado
  redondeado con dos decimales. '''
+peso = int(input("Ingrese su peso un numero entero:"))
+estatura = float(input("Ingrese su estatura en metro un numero flotante"))
+
+
+def imc(peso, estatura):
+    IMC = peso / estatura**2
+    return round(IMC)
+
+
+print(f"El IMC es: {imc(peso, estatura)}")
