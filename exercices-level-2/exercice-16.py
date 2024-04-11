137c1c82af94628fa9529a5100ebd7467a88dec5
GitHalex <calex1731@gmail.com>
2024-04-11 14:25:05 -0400

    ejercicio 16 del nivel 2 terminado

--- exercices-level-2/exercice-16.py
+++ exercices-level-2/exercice-16.py
@@ -2,3 +2,10 @@
 muestre por pantalla la <n> entre <m> da un cociente <c> y un resto <r> donde <n> y <m> son 
 los números introducidos por el usuario, y <c> y <r> son el cociente y el resto de la división
  entera respectivamente. '''
+
+numerador = int(input("NUMERADOR: "))
+denominador = int(input("DENOMINADOR: "))
+
+cociente = numerador/denominador
+residuo = numerador % denominador
+print(f"Cociente: {cociente} ---- Denominador: {residuo}")
