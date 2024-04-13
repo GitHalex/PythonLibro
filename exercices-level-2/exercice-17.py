2965172ca49b00d4eb9c251eb1202a95aafb3506
GitHalex <calex1731@gmail.com>
2024-04-12 20:51:42 -0400

    ejercicio 17 del nivel 2 terminado

--- exercices-level-2/exercice-17.py
+++ exercices-level-2/exercice-17.py
@@ -1,3 +1,9 @@
 '''Escribir un programa que pregunte al usuario una cantidad a invertir, 
 el interés anual y el número de años, y muestre por pantalla 
 el capital obtenido en la inversión.'''
+cantidad_invertir = int(input("Ingrese la cantidad a invertir: "))
+interes_anual = int(input("Interes anual: "))
+number_years = int(input("Numero de años: "))
+
+capital_obtenido = cantidad_invertir*(1+5/100)**number_years
+print(f"Capital obtenido en la inversion: {capital_obtenido}")
