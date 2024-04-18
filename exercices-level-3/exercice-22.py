45f7f172fbd0be029f59f080abbecbc0a255cce0
GitHalex <calex1731@gmail.com>
2024-04-18 14:14:06 -0400

    ejercicio numero 22 del nivel 3 terminado

--- exercices-level-3/exercice-22.py
+++ exercices-level-3/exercice-22.py
@@ -2,3 +2,13 @@
 y después muestre por pantalla el nombre completo del usuario tres veces, una con todas las letras minúsculas, 
 otra con todas las letras mayúsculas y otra solo con la primera letra del nombre y de los apellidos en mayúscula.
  El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera. '''
+
+fullName = input("Please type your full Name: ")
+
+for i in [1, 2, 3]:
+    if i == 1:
+        print(fullName.lower())
+    if i == 2:
+        print(fullName.upper())
+    if i == 3:
+        print(fullName.title())
