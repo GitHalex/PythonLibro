f2141f05e2ff375c09ca75195b5194593795aec0
GitHalex <calex1731@gmail.com>
2024-04-20 19:32:14 -0400

    ejercicio 23 del nivel 3 terminado

--- exercices-level-3/exercice-23.py
+++ exercices-level-3/exercice-23.py
@@ -2,3 +2,6 @@
  después de que el usuario lo introduzca muestre por 
  pantalla <NOMBRE> tiene <n> letras, donde <NOMBRE> es el nombre de usuario en mayúsculas y <n> es el número 
  de letras que tienen el nombre.'''
+
+userName = input("Ingrese el nombre de usuario: ")
+print(f"Name User: {userName.upper()} tiene {len(userName)} letras")
