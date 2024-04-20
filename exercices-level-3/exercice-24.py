b01ca9fe2b0e32f90876f1860dea72e6cc42e25a
GitHalex <calex1731@gmail.com>
2024-04-20 19:37:27 -0400

    ejercicio 24 del nivel 3 terminado estaba muy facil lo resolvi con substring

--- exercices-level-3/exercice-24.py
+++ exercices-level-3/exercice-24.py
@@ -2,3 +2,5 @@
 el prefijo es el código del país +34, y la extensión tiene dos dígitos (por ejemplo +34-913724710-56). 
 Escribir un programa que pregunte por un número de teléfono con este formato y muestre por pantalla el
  número de teléfono sin el prefijo y la extensión.'''
+phoneNumber = input("Please type your phone number con prefijo y extencion: ")
+print(f"{phoneNumber[4:-3]}")
