e3ebf3b153c24b668f55ca58a9633e59a02473a2
GitHalex <calex1731@gmail.com>
2024-04-24 09:57:42 -0400

    agregando el ejercicio 26 terminado del nivel 3

--- exercices-level-3/exercice-26.py
+++ exercices-level-3/exercice-26.py
@@ -1,3 +1,26 @@
 '''Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal,
  y después muestre por pantalla
  la misma frase pero con la vocal introducida en mayúscula'''
+frase = input("Please type a frase: ")
+vocal = input("Ingrese una vocal a,e,i,o,u: ")
+indice = 0
+frase_cambiada = ""
+char = ""
+while indice < len(frase):
+    char = frase[indice]
+    if "a" == char and char == vocal:
+        frase_cambiada += char.upper()
+    elif "e" == char and char == vocal:
+        frase_cambiada += char.upper()
+    elif "i" == char and char == vocal:
+        frase_cambiada += char.upper()
+    elif "o" == char and char == vocal:
+        frase_cambiada += char.upper()
+    elif "u" == char and char == vocal:
+        frase_cambiada += char.upper()
+    else:
+        frase_cambiada += char
+
+    indice += 1
+
+print(frase_cambiada)
