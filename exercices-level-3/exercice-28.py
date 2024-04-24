f207773e97c596aa83c9ebfbd44ce0dd44ac7e22
GitHalex <calex1731@gmail.com>
2024-04-24 17:01:22 -0400

    ejercicio 28 del nivel 3 teminado

--- exercices-level-3/exercice-28.py
+++ exercices-level-3/exercice-28.py
@@ -1,3 +1,14 @@
 '''Escribir un programa que pregunte por consola el precio de un
  producto en euros con dos decimales y muestre por pantalla el número de euros y 
 el número de céntimos del precio introducido.'''
+print("""
+  PAPA
+  CEBOLLA
+  ZANAHORIA
+  CARNE    
+""")
+precio = float(input("Ingrese el precio de un producto: "))
+print(precio)
+print(f"{int(precio)} euros")
+resultado = float(precio) - int(precio)
+print(f"{int(resultado*100)+1} centimos")
