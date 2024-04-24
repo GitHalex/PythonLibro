cab46b5329443f7c4fd139050b2b779848ee48d7
GitHalex <calex1731@gmail.com>
2024-04-24 10:08:16 -0400

    ejercicio 27 del nivel 3 terminado

--- exercices-level-3/exercice-27.py
+++ exercices-level-3/exercice-27.py
@@ -1,3 +1,9 @@
 '''Escribir un programa que pregunte el correo electrónico del usuario en la consola y 
 muestre por pantalla otro correo electrónico con el mismo nombre 
 (la parte delante de la arroba @) pero con dominio ceu.es.'''
+email = input("Ingrese su correo electronico: ")
+posicion = email.find("@")
+print(posicion)
+print(email[0:posicion])
+nuevoDominio = email[0:posicion+1] + "ceu.es"
+print(nuevoDominio)
