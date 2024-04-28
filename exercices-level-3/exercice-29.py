a9f66414292262c4b6686cce0fadbebe287d7c0c
GitHalex <calex1731@gmail.com>
2024-04-28 10:51:03 -0400

    ejercicio 29 terminado

--- exercices-level-3/exercice-29.py
+++ exercices-level-3/exercice-29.py
@@ -2,3 +2,9 @@
 nacimiento en formato dd/mm/aaaa y muestra por pantalla, el día, el mes y el año.
  Adaptar el programa anterior para que también funcione cuando
  el día o el mes se introduzcan con un solo carácter.'''
+
+
+nacimiento = input("Ingrese su flecha de nacimiento en formato dd/mm/aaaa: ")
+print(f"Dia: {nacimiento[0:2]}")
+print(f"Mes: {nacimiento[3:5]}")
+print(f"Año: {nacimiento[6:]}")
