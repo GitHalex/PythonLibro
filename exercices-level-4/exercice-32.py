2f0d037163fc476335e9e73296081797c4fd82e1
GitHalex <calex1731@gmail.com>
2024-04-30 19:08:38 -0400

    ejercicio 32 del nivel 4

--- exercices-level-4/exercice-32.py
+++ exercices-level-4/exercice-32.py
@@ -1,1 +1,9 @@
 '''Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.'''
+
+edad = int(input("Ingrese su edad: "))
+if edad >= 18:
+    print("Es mayor de edad!!!")
+elif edad > 0:
+    print("Es menor de edad!!!")
+else:
+    print("La edad {edad} introducida es 0 o numero negativo")
