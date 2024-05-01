2dc60b52d759c51f1ea228a9902767074948bbe5
GitHalex <calex1731@gmail.com>
2024-05-01 14:34:17 -0400

    Ejerccio 31 de nuvel 4 terminado

--- exercices-level-4/exercice-31.py
+++ exercices-level-4/exercice-31.py
@@ -2,3 +2,12 @@
 su precio y un número de unidades y muestre por pantalla una cadena con el nombre del 
 producto seguido de su precio unitario con 6 dígitos enteros y 2 decimales, el número de unidades
  con tres dígitos y el coste total con 8 dígitos enteros y 2 decimales.'''
+nameProduct = input("PRODUCTO: ")
+# utilizar float en lugar de int para permitir deciamales
+price = int(input("PRECIO: "))
+cantidad = int(input("CANTIDAD: "))
+# calcular el costo total
+total_cost = price
+
+print(
+    f"Nombre del product: {nameProduct}\nPrecio{price:8.2f}\nNumero de unidades: {cantidad:03d}\nCosto total:{total_cost:19.2f}")
