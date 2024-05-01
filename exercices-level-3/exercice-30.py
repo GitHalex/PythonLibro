e7890add49e7ff2818ae7c72b9a4a3da3173a674
GitHalex <calex1731@gmail.com>
2024-05-01 14:05:07 -0400

    ejercicio 30 del nivel 3 temrinado

--- exercices-level-3/exercice-30.py
+++ exercices-level-3/exercice-30.py
@@ -1,3 +1,14 @@
 '''Escribir un programa que pregunte por consola por los productos de una cesta de la compra,
  separados por comas, y muestre por pantalla
  cada uno de los productos en una línea distinta.'''
+productos = input("""
+Productos de compra separados por comas:
+""")
+
+# Dividir la entrada por comas
+lista_productos = productos.split(',')
+
+# Mostrar cada producto en una línea distinta
+for producto in lista_productos:
+    # Utiliza strip() para eliminar los espacios en blanco alrededor de cada producto
+    print(producto.strip())
