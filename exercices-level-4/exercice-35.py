aabe18b6be9bb5f2a1906a1b27829774db5a6bf7
GitHalex <calex1731@gmail.com>
2024-05-06 20:34:47 -0400

    ejercicio 35 terminado del nivel 4

--- exercices-level-4/exercice-35.py
+++ exercices-level-4/exercice-35.py
@@ -1,1 +1,6 @@
 '''Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.'''
+numero = int(input("Ingrese un numero: "))
+if numero % 2 == 0:
+    print(f"El: {numero} es par")
+else:
+    print(f"El: {numero} es impar")
