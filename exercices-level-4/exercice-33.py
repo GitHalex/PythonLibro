553df66672a7a35c7dc4805d924a182ced053c2c
GitHalex <calex1731@gmail.com>
2024-05-06 20:32:23 -0400

    ejercicio 33 del nivel 4 terminado

--- exercices-level-4/exercice-33.py
+++ exercices-level-4/exercice-33.py
@@ -1,3 +1,11 @@
 '''Escribir un programa que almacene la cadena de caracteres contraseña 
 en una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña 
 introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas. '''
+
+password = "hola1"
+passWord_usuario = input("Cual es la contraseña: ")
+
+if password == passWord_usuario.lower():
+    print("la contraseña es correcta")
+else:
+    print("La contraseña no es la correcta")
