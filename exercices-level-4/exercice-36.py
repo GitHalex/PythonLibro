1acc4aa52f4ff5328e77ab4ca2a4ad98b8529695
GitHalex <calex1731@gmail.com>
2024-05-07 10:39:21 -0400

    ejercicio 36 del nivel 4 terminado

--- exercices-level-4/exercice-36.py
+++ exercices-level-4/exercice-36.py
@@ -1,3 +1,13 @@
 '''Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos 
 iguales o superiores a 1000 € mensuales. Escribir un programa que pregunte al usuario su edad y sus 
 ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no. '''
+
+edad = int(input("Edad: "))
+ingresos = int(input("Ingresos mensuales: "))
+
+if edad >= 16 and ingresos >= 1000:
+    print(f"tienes: {edad} años -- Ingreso: {ingresos} bs")
+    print("Debes Tirbutar")
+else:
+    print(
+        f"No puedes tributar ya sea por tu edad: {edad} o por tu ingreso economico: {ingresos}")
