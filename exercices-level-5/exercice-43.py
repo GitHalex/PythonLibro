bb8e8a52288914f06d7786bf384c222e42ee9d6f
GitHalex <calex1731@gmail.com>
2024-05-10 17:38:05 -0400

    ejercicio 43 del nivel 5 terminado

--- exercices-level-5/exercice-43.py
+++ exercices-level-5/exercice-43.py
@@ -1,2 +1,10 @@
 '''Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla
  la cuenta atrás desde ese número hasta cero separados por comas.'''
+
+numero = int(input("Ingrese un numero entero positivo: "))
+numeros = []
+while numero > 0:
+    numeros.append(numero)
+    numero -= 1
+
+print(*numeros, sep=",")
