743c0b1263610d7cbf239e09837ab6d55ba96f0d
GitHalex <calex1731@gmail.com>
2024-05-10 17:35:14 -0400

    ejercicio 42 del nivel 5 terminado

--- exercices-level-5/exercice-42.py
+++ exercices-level-5/exercice-42.py
@@ -1,2 +1,14 @@
 '''Escribir un programa que pida al usuario un número entero positivo
  y muestre por pantalla todos los números impares desde 1 hasta ese número separados por comas. '''
+
+numero = int(input("Ingrese un numero entero positivo: "))
+inicio = 1
+impares = []  # Lista para almacenar los números impares
+
+while inicio <= numero:
+    if inicio % 2 != 0:
+        impares.append(inicio)  # Agrega el número impar a la lista
+    inicio += 1
+
+# Imprime los números impares separados por comas
+print(*impares, sep=",")
