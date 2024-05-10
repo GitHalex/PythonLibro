046b07deeb6637fcea1fb81b8f926b2e559a840a
GitHalex <calex1731@gmail.com>
2024-05-10 16:50:29 -0400

    ejercicio 41 del nivel 5 terminado

--- exercices-level-5/exercice-41.py
+++ exercices-level-5/exercice-41.py
@@ -1,2 +1,8 @@
 '''Escribir un programa que pregunte al usuario su edad y muestre por 
 pantalla todos los años que ha cumplido (desde 1 hasta su edad).'''
+
+edad = int(input("Enter your age: "))
+i = 1
+while i <= edad:
+    print(f"años que has cumplido: {i}")
+    i += 1
