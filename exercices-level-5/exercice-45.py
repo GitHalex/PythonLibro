35e153b2aa6a55585a9d02424118cacc0c828c9d
GitHalex <calex1731@gmail.com>
2024-05-12 20:48:49 -0400

    ejercicio 45 del nivel 5 terminado

--- exercices-level-5/exercice-45.py
+++ exercices-level-5/exercice-45.py
@@ -6,3 +6,10 @@ como el de más abajo, de altura el número introducido.
 ***
 ****
 ***** '''
+cantidad = int(input("Ingrese un numero: "))
+indice = 1
+caracter = ""
+while indice <= cantidad:
+    caracter += "#"
+    print(caracter)
+    indice += 1
