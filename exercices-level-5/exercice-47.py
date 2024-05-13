1a3ff604c33bf63c04de34904c1bf3e09e7fb9be
GitHalex <calex1731@gmail.com>
2024-05-13 19:40:59 -0400

    ejercicio 47 del nivel 5 terminado

--- exercices-level-5/exercice-47.py
+++ exercices-level-5/exercice-47.py
@@ -6,3 +6,20 @@ como el de más abajo.
 5 3 1
 7 5 3 1
 9 7 5 3 1'''
+
+numero = int(input("Tamaño de triangulo: "))
+
+for i in range(1, numero + 1):
+    if i % 2 != 0:  # Solo procesar números impares
+        for j in range(i, 0, -2):
+            print(j, end=" ")
+        print()  # Nueva línea después de cada fila
+
+
+i = 1
+imprimir = ""
+aux = 1
+while i <= numero:
+    imprimir = str(i) + " " + imprimir
+    print(imprimir)
+    i += 1
