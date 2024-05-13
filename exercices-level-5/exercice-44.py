b136bed9409cea34c151ddf6525d0e91c725fb68
GitHalex <calex1731@gmail.com>
2024-05-12 20:45:35 -0400

    ejercicio 44 del nivel 5 terminado

--- exercices-level-5/exercice-44.py
+++ exercices-level-5/exercice-44.py
@@ -1,2 +1,12 @@
 '''Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el número de años,
  y muestre por pantalla el capital obtenido en la inversión cada año que dura la inversión.'''
+invertir = int(input("Cantidad a invertir: "))
+interes = int(input("Interes anual: "))
+num_year = int(input("Numero de años: "))
+
+capital = invertir*(1+interes/100)**num_year
+print(capital)
+
+for year in range(1, num_year + 1):
+    capital = invertir*(1+interes/100)**year
+    print(f"Año {year}: {capital:.2f}")
