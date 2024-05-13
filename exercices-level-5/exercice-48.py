9bb54b84737d91d284204fa86ab89be082b2bfdc
GitHalex <calex1731@gmail.com>
2024-05-13 19:45:58 -0400

    ejercicio 48 del nivel 5 terminado

--- exercices-level-5/exercice-48.py
+++ exercices-level-5/exercice-48.py
@@ -1,2 +1,11 @@
 '''Escribir un programa que almacene la cadena de caracteres contraseña en una variable, 
 pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta.'''
+
+password = "alex123"
+while True:
+    contrasenia = input("Ingrese una contraseña: ")
+    if password == contrasenia:
+        print("CONTRASEÑA CORRECTA")
+        break
+    else:
+        print("CONTRASEÑA INCORRECTA")
