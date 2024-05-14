00e4072f8bf7bee82bb9afb4c340345c7ee32636
GitHalex <calex1731@gmail.com>
2024-05-14 11:08:04 -0400

    ejercicio 54 del nivel 6 termiando

--- exercices-level-6/exercice-54.py
+++ exercices-level-6/exercice-54.py
@@ -1,3 +1,18 @@
 '''Escribir un programa que almacene las asignaturas de un curso 
 (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla 
 el mensaje Yo estudio <asignatura>, donde <asignatura> es cada una de las asignaturas de la lista. '''
+asignaturas = []
+while True:
+    materias = input("Ingrese una materia: ")
+    if materias == "salir":
+        break
+    else:
+        asignaturas.append(materias)
+
+print(f"""
+#############################################
+  YO ESTUDIO:                       
+      {asignaturas}             
+  es cada una de las asignaturas    
+#############################################
+""")
