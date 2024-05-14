5a3e39ac688c28e01fd72cf0b250e52d53973874
GitHalex <calex1731@gmail.com>
2024-05-14 11:02:29 -0400

    ejercicio 53 del nivel 6 terminado

--- exercices-level-6/exercice-53.py
+++ exercices-level-6/exercice-53.py
@@ -1,2 +1,11 @@
 '''Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, 
 Física, Química, Historia y Lengua) en una lista y la muestre por pantalla. '''
+asignaturas = []
+while True:
+    materias = input("Ingrese una materia: ")
+    if materias == "salir":
+        break
+    else:
+        asignaturas.append(materias)
+
+print(asignaturas)
