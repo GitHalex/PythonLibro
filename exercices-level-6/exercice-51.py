638bad6978ce9485aeab03fdf2d8c0bff20d64bc
GitHalex <calex1731@gmail.com>
2024-05-14 10:56:47 -0400

    ejercicio 51 del nivel 6 terminado

--- exercices-level-6/exercice-51.py
+++ exercices-level-6/exercice-51.py
@@ -1,2 +1,10 @@
 '''Escribir un programa en el que se pregunte al usuario por una frase y una letra, 
 y muestre por pantalla el número de veces que aparece la letra en la frase.'''
+frase = input("Ingrese una frase: ")
+character = input("Ingrese un character para buscar en la frase: ")
+veces = 0
+for char in frase:
+    if character == char:
+        veces += 1
+
+print(f"la Frase: \n{frase}\ntiene {character}\nse repite {veces} veces")
