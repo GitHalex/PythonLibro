669cf37ef2ae30a07109b40d0f64cca37e25df4d
GitHalex <calex1731@gmail.com>
2024-05-16 17:05:55 -0400

    ejercicio 56 del nivel 6 terminado

--- exercices-level-6/exercice-56.py
+++ exercices-level-6/exercice-56.py
@@ -1,2 +1,12 @@
 '''Escribir un programa que pregunte al usuario los números ganadores de 
 la lotería primitiva, los almacene en una lista y los muestre por pantalla ordenados de menor a mayor. '''
+numerosGanadores = []
+while True:
+    number = int(input("Numeros Ganadores: "))
+    print("Para salir ingrese un numero negativo: ")
+    if number < 0:
+        break
+    else:
+        numerosGanadores.append(number)
+numerosGanadores.sort()
+print(numerosGanadores)
