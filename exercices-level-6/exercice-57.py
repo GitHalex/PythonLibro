82891675ac6ba9e1f258636c91363bb269c23dfd
GitHalex <calex1731@gmail.com>
2024-05-17 09:23:56 -0400

    Ejercicio 57 del nivel 6 terminado

--- exercices-level-6/exercice-57.py
+++ exercices-level-6/exercice-57.py
@@ -1,2 +1,17 @@
 '''Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre 
 por pantalla en orden inverso separados por comas.'''
+i = 1
+listaNumeros = []
+while i <= 10:
+    listaNumeros.append(i)
+    i += 1
+
+
+def listaInversa(listaNumeros: list) -> list:
+    tam = len(listaNumeros) - 1
+    while tam >= 0:
+        print(listaNumeros[tam])
+        tam -= 1
+
+
+listaInversa(listaNumeros)
