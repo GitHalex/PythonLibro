25578786191918cdbe2a0f1641219460fc324df2
GitHalex <calex1731@gmail.com>
2024-05-20 19:43:40 -0400

    ejercicio 59 del nivel 6 terminado

--- exercices-level-6/exercice-59.py
+++ exercices-level-6/exercice-59.py
@@ -1,3 +1,11 @@
 '''Escribir un programa que almacene el abecedario en una lista, elimine de la lista 
 las letras que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista resultante.'''
-listaAbecedario = []
+# Crear una lista con el abecedario
+listaAbecedario = list('abcdefghijklmnopqrstuvwxyz')
+
+# Crear una nueva lista excluyendo las posiciones múltiplos de 3
+resultado = [letra for i, letra in enumerate(
+    listaAbecedario) if (i + 1) % 3 != 0]
+
+# Mostrar la lista resultante
+print(resultado)
