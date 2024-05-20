dfad47f065d5e31f618cdf2eafe77256da53b2ad
GitHalex <calex1731@gmail.com>
2024-05-20 19:53:30 -0400

    ejercicios 60 del nivel seis terminado

--- exercices-level-6/exercice-60.py
+++ exercices-level-6/exercice-60.py
@@ -1,1 +1,14 @@
 '''Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo.'''
+
+
+def esPalindromo(cadena: str):
+    reversa = cadena[::-1]
+    return reversa
+
+
+cadena = input("Ingrese una palabra: ")
+
+if cadena == esPalindromo(cadena):
+    print(f"{cadena} = {esPalindromo(cadena)} es 'PALINDROMO'")
+else:
+    print(f"{cadena} = {esPalindromo(cadena)} no es 'PALINDROMO'")
