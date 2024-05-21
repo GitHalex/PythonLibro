2df052085ac82bc03db0d8116d137319e2526d21
GitHalex <calex1731@gmail.com>
2024-05-21 14:54:53 -0400

    ejercicio 62 del nivel 7 terminado

--- exercices-level-7/exercice-62.py
+++ exercices-level-7/exercice-62.py
@@ -1,2 +1,14 @@
 '''Escribir un programa que almacene en una lista los siguientes 
 precios, 50, 75, 46, 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios.'''
+listaPrecios = [50, 75, 46, 22, 80, 65, 8]
+print(listaPrecios)
+menor = listaPrecios[0]
+mayor = listaPrecios[0]
+for num in listaPrecios:
+    if num < menor:
+        menor = num
+    if num > mayor:
+        mayor = num
+
+print(f"{menor} es el numero menor")
+print(f"{mayor} es el numero mayor")
