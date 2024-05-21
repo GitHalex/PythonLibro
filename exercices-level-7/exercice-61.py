737ec922a1de2b7993e07213cf945005ee820fab
GitHalex <calex1731@gmail.com>
2024-05-20 20:22:04 -0400

    ejercicio 61 terminado del nivel 7

--- exercices-level-7/exercice-61.py
+++ exercices-level-7/exercice-61.py
@@ -1,2 +1,31 @@
 '''Escribir un programa que pida al usuario una palabra y muestre por pantalla
  el número de veces que contiene cada vocal.'''
+palabra = input("Ingrese una palabra: ")
+cantA = 0
+cantE = 0
+cantI = 0
+cantO = 0
+cantU = 0
+for char in palabra:
+    minusculaChar = char.lower()
+    if minusculaChar == "a":
+        cantA += 1
+    if minusculaChar == "e":
+        cantE += 1
+    if minusculaChar == "i":
+        cantI += 1
+    if minusculaChar == "o":
+        cantO += 1
+    if minusculaChar == "u":
+        cantU += 1
+
+if cantA > 0:
+    print(f"catidad de a: {cantA}")
+if cantE > 0:
+    print(f"catidad de e: {cantE}")
+if cantI > 0:
+    print(f"catidad de i: {cantI}")
+if cantO > 0:
+    print(f"catidad de o: {cantO}")
+if cantU > 0:
+    print(f"catidad de u: {cantU}")
