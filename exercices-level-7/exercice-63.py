cca367b9d4bcbad14e59506983ed2191c2c848d8
GitHalex <calex1731@gmail.com>
2024-06-27 16:12:58 -0400

    Ejercicio 63 esta terminado

--- exercices-level-7/exercice-63.py
+++ exercices-level-7/exercice-63.py
@@ -1,2 +1,14 @@
 '''Escribir un programa que almacene los vectores (1,2,3) y (-1,0,2) en dos listas y 
 muestre por pantalla su producto escalar.'''
+lista1 = [1, 2, 3]
+lista2 = [-1, 0, 2]
+
+# Inicializar el producto escalar en 0
+producto_escalar = 0
+
+# Calcular el producto escalar
+for i in range(len(lista1)):
+    producto_escalar += lista1[i] * lista2[i]
+
+# Imprimir el resultado
+print(producto_escalar)
