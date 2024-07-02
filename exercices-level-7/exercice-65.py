46da5bf0140d24098d7fc2ee967cff4434c6dbe2
GitHalex <calex1731@gmail.com>
2024-07-01 22:24:56 -0400

    Ejercico o

--- exercices-level-7/exercice-65.py
+++ exercices-level-7/exercice-65.py
@@ -1,3 +1,18 @@
 '''Escribir un programa que guarde en una variable el
- diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al 
+ diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al
  usuario por una divisa y muestre su símbolo o un mensaje de aviso si la divisa no está en el diccionario.'''
+
+
+def consulta(divisaDict: dict) -> None:
+    clave = input("Ingrese una divisa: Euro-Dollar-Yen :")
+    for moneda in divisaDict:
+        if clave == moneda:
+            print(f"{moneda} => {divisaDict[moneda]}")
+            break
+        else:
+            print(f"No existe divisa: {clave} en  {divisaDict}")
+            break
+
+
+divisa = {'Euro': '€', 'Dollar': '$', 'Yen': '¥'}
+consulta(divisa)
