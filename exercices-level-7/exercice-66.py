f76cbf2cefcf4a7b7deaa01dffb897c659b66ae2
GitHalex <calex1731@gmail.com>
2024-07-02 20:41:02 -0400

    Ejercicio donde usamos un diccionario y lo imprimimos

--- exercices-level-7/exercice-66.py
+++ exercices-level-7/exercice-66.py
@@ -1,3 +1,13 @@
 '''Escribir un programa que pregunte al usuario su nombre, edad, dirección y teléfono y
  lo guarde en un diccionario. Después debe mostrar por pantalla el mensaje <nombre> tiene <edad> años,
   vive en <dirección> y su número de teléfono es <teléfono>.'''
+datos = {}
+nombre = input("Ingrese un nombre: ")
+edad = int(input("Ingrese su edad: "))
+direccion = input("Ingrese su direccion: ")
+telefeno = int(input("Ingrese su celular: "))
+datos[nombre] = (edad, direccion, telefeno)
+print(datos)
+for name in datos:
+    print(
+        f"{name} tiene: {datos[name][0]} años vive en: {datos[name][1]} y su numero de telefono es: {datos[name][2]}")
