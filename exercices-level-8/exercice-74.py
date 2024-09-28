157882c6cd6b5e3f52e5ebd7933125de975f8169
GitHalex <calex1731@gmail.com>
2024-09-28 19:16:29 -0400

    Agregando nuevas funcionalidades

--- exercices-level-8/exercice-74.py
+++ exercices-level-8/exercice-74.py
@@ -1,1 +1,13 @@
 '''Escribir una función a la que se le pase una cadena <nombre> y muestre por pantalla el saludo ¡hola <nombre>!.'''
+
+import tkinter as tk
+
+
+def prueba_tkinter():
+    ventana = tk.Tk()
+    ventana.title("Prueba de Tkinter")
+    tk.Label(ventana, text="¡Tkinter está funcionando en Ubuntu!").pack(pady=20)
+    ventana.mainloop()
+
+
+prueba_tkinter()
