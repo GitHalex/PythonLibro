63ed921173bcce330466291f0ed94ed543b5be2a
GitHalex <calex1731@gmail.com>
2025-03-17 23:54:44 -0400

    Added a new execerce

--- exercices-level-8/exercice-77.py
+++ exercices-level-8/exercice-77.py
@@ -1,3 +1,11 @@
 '''Escribir una función que calcule el área de un círculo y otra que calcule 
 el volumen de un cilindro usando la primera función.
 '''
+
+
+def area(radio: int) -> int:
+    return 3.1416 * radio ** 2
+
+
+resultado = area(5)
+print(resultado)
